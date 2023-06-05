d1a00380e4272cff6bb92c2cebf67a45de2b39a7
Ostap Onyshkiv <prostoostap58@gmail.com>
2023-06-05 11:58:09 +0300

    added data base

--- src/main/resources/library_final_project_script.sql
+++ src/main/resources/library_final_project_script.sql
@@ -1,4 +1,3 @@
--- MySQL Workbench Forward Engineering
 
 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
@@ -46,7 +45,7 @@ DROP TABLE IF EXISTS `library_final_project`.`user` ;
 CREATE TABLE IF NOT EXISTS `library_final_project`.`user` (
   `login` VARCHAR(45) NOT NULL,
   `email` VARCHAR(255) NOT NULL,
-  `password` VARCHAR(32) NOT NULL,
+  `password` VARCHAR(255) NOT NULL,
   `role_id` INT NOT NULL,
   `status_id` INT NOT NULL,
   `first_name` VARCHAR(45) NOT NULL,
@@ -86,20 +85,21 @@ CREATE TABLE IF NOT EXISTS `library_final_project`.`publication` (
 DROP TABLE IF EXISTS `library_final_project`.`book` ;
 
 CREATE TABLE IF NOT EXISTS `library_final_project`.`book` (
-  `isbn` INT NOT NULL,
+  `isbn` VARCHAR(30) NOT NULL,
   `name` VARCHAR(255) NOT NULL,
   `date_of_publication` DATE NOT NULL,
   `publication_id` INT NOT NULL,
   `quantity` INT NOT NULL DEFAULT 1,
   `details` VARCHAR(5000) NULL,
+  `is_active` boolean default true,
   PRIMARY KEY (`isbn`),
   UNIQUE INDEX `isbn_UNIQUE` (`isbn` ASC) VISIBLE,
   INDEX `fk_book_publication1_idx` (`publication_id` ASC) VISIBLE,
   CONSTRAINT `fk_book_publication1`
     FOREIGN KEY (`publication_id`)
     REFERENCES `library_final_project`.`publication` (`publication_id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION);
+    ON DELETE CASCADE
+    ON UPDATE CASCADE);
 
 
 -- -----------------------------------------------------
@@ -113,14 +113,13 @@ CREATE TABLE IF NOT EXISTS `library_final_project`.`authors` (
   PRIMARY KEY (`authors_id`),
   CONSTRAINT UC_Name UNIQUE (name));
 
-
 -- -----------------------------------------------------
 -- Table `library_final_project`.`book_has_authors`
 -- -----------------------------------------------------
 DROP TABLE IF EXISTS `library_final_project`.`book_has_authors` ;
 
 CREATE TABLE IF NOT EXISTS `library_final_project`.`book_has_authors` (
-  `b_isbn` INT NOT NULL,
+  `b_isbn` VARCHAR(30) NOT NULL,
   `a_id` INT NOT NULL,
   PRIMARY KEY (`b_isbn`, `a_id`),
   INDEX `fk_book_has_authors_authors1_idx` (`a_id` ASC) VISIBLE,
@@ -128,13 +127,13 @@ CREATE TABLE IF NOT EXISTS `library_final_project`.`book_has_authors` (
   CONSTRAINT `fk_book_has_authors_book1`
     FOREIGN KEY (`b_isbn`)
     REFERENCES `library_final_project`.`book` (`isbn`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
+    ON DELETE CASCADE
+    ON UPDATE CASCADE,
   CONSTRAINT `fk_book_has_authors_authors1`
     FOREIGN KEY (`a_id`)
     REFERENCES `library_final_project`.`authors` (`authors_id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION);
+    ON DELETE CASCADE
+    ON UPDATE CASCADE);
 
 
 -- -----------------------------------------------------
@@ -149,16 +148,6 @@ CREATE TABLE IF NOT EXISTS `library_final_project`.`subscription_status` (
   CONSTRAINT UC_Name UNIQUE (name));
 
 
--- -----------------------------------------------------
--- Table `library_final_project`.`way_of_using`
--- -----------------------------------------------------
-DROP TABLE IF EXISTS `library_final_project`.`way_of_using` ;
-
-CREATE TABLE IF NOT EXISTS `library_final_project`.`way_of_using` (
-  `way_of_using_id` INT NOT NULL AUTO_INCREMENT,
-  `name` VARCHAR(45) NOT NULL,
-  PRIMARY KEY (`way_of_using_id`),
-  CONSTRAINT UC_Name UNIQUE (name));
 
 
 -- -----------------------------------------------------
@@ -168,59 +157,32 @@ DROP TABLE IF EXISTS `library_final_project`.`active_book` ;
 
 CREATE TABLE IF NOT EXISTS `library_final_project`.`active_book` (
   `active_book_id` INT NOT NULL AUTO_INCREMENT,
-  `book_isbn` INT NOT NULL,
-  `way_of_using_id` INT NOT NULL,
+  `book_isbn` VARCHAR(30) NOT NULL,
+  `user_login` VARCHAR(45) NOT NULL,
   `subscription_status_id` INT NOT NULL,
-  `start_date` DATETIME NOT NULL DEFAULT now(),
-  `end_date` DATETIME NOT NULL DEFAULT (date_format(`start_date`,'%y-%m-%d 18-00-00')),
+  `start_date` DATE NOT NULL DEFAULT (date_format(now(),'%y-%m-%d')),
+  `end_date` DATE NOT NULL DEFAULT (PERIOD_ADD(date_format(`start_date`,'%y%m'),1)),
   `fine` DECIMAL(9,2) NULL,
   PRIMARY KEY (`active_book_id`),
   UNIQUE INDEX `active_book_id_UNIQUE` (`active_book_id` ASC) VISIBLE,
   INDEX `fk_active_book_subscription_status1_idx` (`subscription_status_id` ASC) VISIBLE,
-  INDEX `fk_active_book_way_of_using1_idx` (`way_of_using_id` ASC) VISIBLE,
   INDEX `fk_active_book_book1_idx` (`book_isbn` ASC) VISIBLE,
+  INDEX `fk_active_book_user_idx` (`user_login` ASC) VISIBLE,
   CONSTRAINT `fk_active_book_subscription_status1`
     FOREIGN KEY (`subscription_status_id`)
     REFERENCES `library_final_project`.`subscription_status` (`subscription_status_id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
-  CONSTRAINT `fk_active_book_way_of_using1`
-    FOREIGN KEY (`way_of_using_id`)
-    REFERENCES `library_final_project`.`way_of_using` (`way_of_using_id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
   CONSTRAINT `fk_active_book_book1`
     FOREIGN KEY (`book_isbn`)
     REFERENCES `library_final_project`.`book` (`isbn`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION);
-
-
--- -----------------------------------------------------
--- Table `library_final_project`.`active_book_has_user`
--- -----------------------------------------------------
-DROP TABLE IF EXISTS `library_final_project`.`active_book_has_user` ;
-
-CREATE TABLE IF NOT EXISTS `library_final_project`.`active_book_has_user` (
-  `active_book_id` INT NOT NULL,
-  `user_login` VARCHAR(45) NOT NULL,
-  PRIMARY KEY (`active_book_id`, `user_login`),
-  INDEX `fk_active_book_has_user_user1_idx` (`user_login` ASC) VISIBLE,
-  INDEX `fk_active_book_has_user_active_book1_idx` (`active_book_id` ASC) VISIBLE,
-  CONSTRAINT `fk_active_book_has_user_active_book1`
-    FOREIGN KEY (`active_book_id`)
-    REFERENCES `library_final_project`.`active_book` (`active_book_id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_active_book_has_user_user1`
+    ON DELETE cascade
+    ON UPDATE cascade,
+  CONSTRAINT `fk_active_book_user`
     FOREIGN KEY (`user_login`)
     REFERENCES `library_final_project`.`user` (`login`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION);
-    
-SET SQL_MODE=@OLD_SQL_MODE;
-SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
-SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
+    ON DELETE cascade
+    ON UPDATE cascade);
 
 INSERT INTO `role` (`name`)
 values('reader'),
@@ -229,13 +191,9 @@ values('reader'),
 
 INSERT INTO user_status(name)
 VALUES
-('blocked'),
-('active');
+('active'),
+('blocked');
 
-INSERT INTO way_of_using(name)
-VALUES
-('subscription'),
-('reading room');
 
 INSERT INTO subscription_status(name)
 VALUES
@@ -245,42 +203,71 @@ VALUES
 ('waiting');
 
 
-Insert into book values
-(1,'book1','11-11-11',1,1,null),
-(2,'book2','11-11-11',1,2,null);
+INSERT INTO `authors` (`name`)
+VALUES
+('J.K. Rowling'),
+('George R.R. Martin'),
+('Stephen King'),
+('Robert Jordan'),
+('Brandon Sanderson');
 
-insert into publication values
-(default,'publication1');
+INSERT INTO `publication` (`name`)
+VALUES
+('Bloomsbury Publishing'),
+('Bantam Spectra'),
+('Scribner'),
+('Tor Books');
 
-insert into authors 
-values
-(default,'author1'),
-(default,'author2');
+INSERT INTO `book` (`isbn`, `name`, `date_of_publication`, `publication_id`, `quantity`, `details`, `is_active`)
+VALUES
+('9781408855652', 'Harry Potter and the Sorcerer\'s Stone', '1997-06-26', 1, 10, 'The first book in the Harry Potter series.', 1),
+('9780553801477', 'A Game of Thrones', '1996-08-01', 2, 5, 'The first book in the A Song of Ice and Fire series.', 1),
+('9781501142970', 'It', '1986-09-15', 3, 3, 'A horror novel by Stephen King.', 1),
+('9780312850098', 'The eye of the world', '1990-01-15', 4, 2, 'A high fantasy series by Robert Jordan and Brandon Sanderson.', 1),
+('9780439064866', 'Harry Potter and the Chamber of Secrets', '1998-07-02', 1, 7, 'The second book in the Harry Potter series.', 1),
+('9788831000161', 'Harry Potter and the Prisoner of Azkaban', '1999-07-08', 1, 6, 'The third book in the Harry Potter series.', 1),
+('9788893819930', 'Harry Potter and the Goblet of Fire', '2000-07-08', 1, 8, 'The fourth book in the Harry Potter series.', 1);
 
-insert into book_has_authors values
-(1,2),
-(1,1),
-(2,2);
+INSERT INTO `book_has_authors` (`b_isbn`, `a_id`)
+VALUES
+('9781408855652', 1),
+('9780553801477', 2),
+('9781501142970', 3),
+('9780312850098', 4),
+('9780312850098', 5),
+('9780439064866', 1),
+('9788831000161', 1),
+('9788893819930', 1);
 
-Insert into user values
-('user', 'user@gmail.com','password',1,1,'User','First',default);
+Insert into user (login,email,password,role_id,status_id,first_name,last_name,phone) VALUES
+('admin','123@gmail.com','$argon2id$v=19$m=15360,t=2,p=1$vAgNIdLCePlvli1xUckGgnEc0phIdIca/7J55Szl8DE$JmHKkv2NpJgw7mhNwwcYWoICa0BANR7tUIZvk8b7YmpHkX1MQxNvVcwPOLRy5hLFLC28DagGAF8zJRIljmZyqQ',3,1,'Admin','Admin','0988255564'),
+('user1','user1@gmail.com','$argon2id$v=19$m=15360,t=2,p=1$vAgNIdLCePlvli1xUckGgnEc0phIdIca/7J55Szl8DE$JmHKkv2NpJgw7mhNwwcYWoICa0BANR7tUIZvk8b7YmpHkX1MQxNvVcwPOLRy5hLFLC28DagGAF8zJRIljmZyqQ',1,1,'Tom','Krit',null),
+('blockedUser','blockedUser@gmail.com','$argon2id$v=19$m=15360,t=2,p=1$vAgNIdLCePlvli1xUckGgnEc0phIdIca/7J55Szl8DE$JmHKkv2NpJgw7mhNwwcYWoICa0BANR7tUIZvk8b7YmpHkX1MQxNvVcwPOLRy5hLFLC28DagGAF8zJRIljmZyqQ',1,2,'John','Tork','0674521156'),
+('user2','user2@gmail.com','$argon2id$v=19$m=15360,t=2,p=1$vAgNIdLCePlvli1xUckGgnEc0phIdIca/7J55Szl8DE$JmHKkv2NpJgw7mhNwwcYWoICa0BANR7tUIZvk8b7YmpHkX1MQxNvVcwPOLRy5hLFLC28DagGAF8zJRIljmZyqQ',1,1,'Jean','Boros','0985644475'),
+('librarian1','librarian1@gmail.com','$argon2id$v=19$m=15360,t=2,p=1$vAgNIdLCePlvli1xUckGgnEc0phIdIca/7J55Szl8DE$JmHKkv2NpJgw7mhNwwcYWoICa0BANR7tUIZvk8b7YmpHkX1MQxNvVcwPOLRy5hLFLC28DagGAF8zJRIljmZyqQ',2,1,'Vitalii','Tort','0985625675'),
+('librarian2','librarian2@gmail.com','$argon2id$v=19$m=15360,t=2,p=1$vAgNIdLCePlvli1xUckGgnEc0phIdIca/7J55Szl8DE$JmHKkv2NpJgw7mhNwwcYWoICa0BANR7tUIZvk8b7YmpHkX1MQxNvVcwPOLRy5hLFLC28DagGAF8zJRIljmZyqQ',2,1,'Max','Verst','0985644145'),
+('librarian3','librarian3@gmail.com','$argon2id$v=19$m=15360,t=2,p=1$vAgNIdLCePlvli1xUckGgnEc0phIdIca/7J55Szl8DE$JmHKkv2NpJgw7mhNwwcYWoICa0BANR7tUIZvk8b7YmpHkX1MQxNvVcwPOLRy5hLFLC28DagGAF8zJRIljmZyqQ',2,1,'Israel','Ezze','0685254145');
+
+Insert into active_book (book_isbn,user_login,subscription_status_id,start_date,end_date,fine) values
+('9781408855652','user1',1,'2023-06-05','2023-06-30',100),
+('9780553801477','user1',3,'2023-05-05','2023-06-01',100),
+('9788893819930','user1',1,'2023-06-01','2023-07-15',100),
+('9788831000161','user1',4,'2023-06-01','2023-06-01',null),
+('9780312850098','user2',2,'2023-05-01','2023-06-01',100),
+('9780553801477','user2',1,'2023-06-01','2023-06-20',100),
+('9788831000161','user2',3,'2023-05-01','2023-05-30',100),
+('9780439064866','user2',4,'2023-06-01','2023-06-01',null),
+('9781501142970','user2',1,'2023-06-03','2023-06-25',100),
+('9781501142970','blockedUser',2,'2023-03-01','2023-05-15',100);
+
+SET FOREIGN_KEY_CHECKS=1;
 
 
-Insert into user values
-('administrator', 'administrator@gmail.com','password',3,1,'Administrator','Administrator',default);
 
-Insert Into active_book values
-(default,1,1,1,'2022-11-20','2022-12-15',null);
 
-Insert Into active_book values
-(default,2,1,1,'2022-11-20','2022-12-15',null);
 
-insert into active_book_has_user
-values
-(2,'user');
 
-insert into active_book_has_user values
-(1,'user');
+
 
 
 
